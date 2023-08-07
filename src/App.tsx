68bca649c2cdfdfe58c097e27ebe77ad7334a4a7
mahmudulmurad <mahmudul.murad7@gmail.com>
2023-08-07 23:36:22 +0600

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { DiRedhat } from "react-icons/di";
+import { DiMozilla, DiTerminal, DiGitBranch } from "react-icons/di";
+import { FaEarthAmericas } from 'react-icons/fa6'
+import { GiRotaryPhone } from 'react-icons/gi'
 const App = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-8 h-screen">
@@ -8,23 +10,50 @@ const App = () => {
       >
         <div className="flex items-center justify-center p-2">
           <div className="ring ring-emerald-500 rounded-full">
-            <DiRedhat className="text-emerald-500 text-5xl m-2" />
+            <DiMozilla className="text-emerald-500 text-5xl m-2" />
           </div>
         </div>
         <div className="text-sm">Mahmudul Murad</div>
-        <div className="text-xs">mahmudulmurad@gmail.com</div>
+        <div className="text-xs">mahmudul.murad7@gmail.com</div>
       </div>
-      <div className="flex flex-col bg-slate-700 text-white shadow-lg shadow-gray-600 rounded-md p-4 w-96 h-48">
-        <div className="text-xs my-2 text-center">
-          You talking to me? -Taxi Driver, 1976
-        </div>
-        <div className="flex justify-center items-center">
-          <div>1</div>
-          <div className="flex justify-end items-center mx-5">
-            <div>
-              <DiRedhat className="text-2xl" />
+      <div className="flex flex-col justify-center items-center
+            bg-slate-700 text-white shadow-lg shadow-gray-600
+              rounded-md p-4 w-96 h-48">
+        <div className="text-xs my-2 text-center">https://tinyurl.com/24j9zyut</div>
+        <div className="w-full inline-flex">
+          <div className="w-2/5"></div>
+          <div className="flex flex-col justify-start gap-1 w-3/5">
+
+            <div className="flex justify-start items-center mx-5">
+              <div>
+                <DiTerminal className="text-emerald-500 text-2xl" />
+              </div>
+              <div className="text-xs mx-2">software engineer</div>
             </div>
-            <div className="text-xs mx-2">Earth, Milky Way</div>
+
+
+            <div className="flex justify-start items-center mx-5">
+              <div>
+                <DiGitBranch className="text-emerald-500 text-2xl" />
+              </div>
+              <div className="text-xs mx-2">mahmudulmurad.github.io</div>
+            </div>
+
+
+
+            <div className="flex justify-start items-center mx-5">
+              <div>
+                <GiRotaryPhone className="text-emerald-400 text-2xl" />
+              </div>
+              <div className="text-xs mx-2">+8801###359136</div>
+            </div>
+
+            <div className="flex justify-start items-center mx-5">
+              <div>
+                <FaEarthAmericas className="text-emerald-500 text-2xl" />
+              </div>
+              <div className="text-xs mx-2">earth, milky way</div>
+            </div>
           </div>
         </div>
       </div>
