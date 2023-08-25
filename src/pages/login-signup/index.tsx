d21eebc63d41354b1867c5a4f60c05b0528192f5
mahmudulmurad <mahmudul.murad7@gmail.com>
2023-08-25 23:20:12 +0600

    added some design for login signup page

--- src/pages/login-signup/index.tsx
+++ src/pages/login-signup/index.tsx
@@ -1,7 +1,12 @@
+import Box from "@mui/material/Box";
 import React from "react";
 
 const LoginSignup = () => {
-  return <div>Login Signup</div>;
+  return (
+    <Box className="flex justify-center items-center h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500">
+      <Box className=" h-1/2 w-1/4 bg-white drop-shadow-md">Login Signup</Box>
+    </Box>
+  );
 };
 
 export default LoginSignup;
